4a236fa9bdea9cf51fefb2c7fd2aa9e369769b62
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2022-11-30 15:55:33 -0600

    se agrega un campo al regitro de quotation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Customer.create(code: "AEM-01", email: "aeomexico01@amexico.com", fullName: "Aer
 Contact.create(code: "C001", fullName: "Eli Granados Lopez", email: "egranadoslopez@amexico.com", phoneNumber: "5533458977 e233", customer_id: Customer.last.id)
 
 Quotation.create(code: "20220126_AEROMEXICO_AF-163-20ST_PACM", quotationDate: Date.today, deliveryTerm: "DDP (Mexican Territory)", minimumOrder: "See Refere
-nce below", leadTime: "See reference below", customer_id: am.id, consultant_id: Consultant.last.id, expirationDays: 30, creditDays: 30)
+nce below", leadTime: "See reference below", customer_id: am.id, consultant_id: Consultant.last.id, expirationDays: 30, creditDays: 30, contact_id: Contact.last.id)
 
 QuotationDetail.create(quotation_id: Quotation.last.id, good_id: Good.last.id, quantity: 2, leadTime: '110-130',
                        sellingUnit: 'KIT', price: 2715.72, discount: 0)
